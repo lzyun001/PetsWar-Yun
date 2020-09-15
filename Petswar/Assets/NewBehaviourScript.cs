473127427da5e36b0d27f1ae0488531705fc49df
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-09-15 20:47:32 +0800

    running done

---
+++ Petswar/Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using KID;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Button a;
+    IEnumerator Start()
+    {
+        string b = RandomScene.LoadRandomScene();
+        print(b);
+        if (GameObject.FindGameObjectsWithTag("Button").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        yield return null;
+
+        a.onClick.AddListener(() =>
+        {
+            string a = RandomScene.LoadRandomScene();
+            print(a);
+        }
+        );
+    }
+}
